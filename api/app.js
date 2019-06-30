3701b2ac2f79db3f2faddf7817244707cdd0adf9
DCU4 <DylanJConnor4@gmail.com>
2019-06-30 16:37:57 -0400

    change port

--- api/app.js
+++ api/app.js
@@ -129,11 +129,11 @@ var server = http.createServer(app);
 //     console.log('Server started');
 // });
 
-app.listen(process.env.PORT, process.env.IP, function () {
-    console.log('Server started');
-});
-
-// app.listen(process.env.PORT || 3000, function(){
+// app.listen(process.env.PORT, process.env.IP, function () {
 //     console.log('Server started');
 // });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Server started');
+});
 
