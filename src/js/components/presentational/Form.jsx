e701e630509827155dacc085743f7882ae067101
Dylan Connor <dconnor@req.co>
2019-07-13 18:45:50 -0400

    set up form so it doesnt submit if string is empty

--- src/js/components/presentational/Form.jsx
+++ src/js/components/presentational/Form.jsx
@@ -8,10 +8,11 @@ export default class Form extends Component {
   }
 
   render() {
+    let note = this.props.note
 
     return (
-      <form id="addNote" className={this.props.class} onSubmit={this.props.writeNote}>
-        <textarea value={this.props.note} onChange={this.props.onChange}></textarea>
+      <form id="addNote" className={this.props.class} onSubmit={note!=="" ? this.props.writeNote : null}>
+        <textarea value={note} onChange={this.props.onChange}></textarea>
       </form>
     );
   }
